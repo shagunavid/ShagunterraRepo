208501e01cb0cd47c72fca322b85d62e3214bba5
shagunavid <shagun@avidsecure.io>
2018-12-06 17:34:57 +0530

    shagun

--- demoCT.tf
+++ demoCT.tf
@@ -199,52 +199,6 @@ resource "aws_iam_account_password_policy" "strict_2" {
   allow_users_to_change_password = true
 }
 
-resource "aws_cloudtrail" "example" {
-
-  is_multi_region_trail = true
-
-  cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
-  event_selector {
-    read_write_type = "All"
-    include_management_events = true
-
-    data_resource {
-      type   = "AWS::Lambda::Function"
-      values = ["arn:aws:lambda"]
-    }
-  }
-}
-resource "aws_cloudtrail" "example2" {
-
-  is_multi_region_trail = true
-
- # cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
-  event_selector {
-    read_write_type = "All"
-    include_management_events = true
-
-    data_resource {
-      type   = "AWS::Lambda::Function"
-      values = ["arn:aws:lambda"]
-    }
-  }
-}
-resource "aws_cloudtrail" "example3" {
-
-  is_multi_region_trail = true
-
-  #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
-  event_selector {
-    read_write_type = "All"
-    include_management_events = true
-
-    data_resource {
-      type   = "AWS::Lambda::Function"
-      values = ["arn:aws:lambda"]
-    }
-  }
-}
-
 
 
 #Ensure a log metric filter and alarm exist for Management Console sign-in without MFA
